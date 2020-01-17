bddc436a02a9842faee47ac03759b45073ddc360
Paweł Duda <pduda.98@gmail.com>
2020-01-17 19:56:50 +0100

    dokończenie włącznika testów

--- iscom/iscom/iscom.cpp
+++ iscom/iscom/iscom.cpp
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #endif
 #include "Core.h"
+#include "Test.h"
 
 
 void SetStdinEcho(bool enable = true)
@@ -42,6 +43,80 @@ void SetStdinEcho(bool enable = true)
 
 void tests() {
 
+	//testy dla metod z klasy Admin
+
+	if (!Test::testAddMod()) {
+		std::cout << Test::testAddMod() << std::endl;
+	}
+	if (!Test::testRemoveMod()) {
+		std::cout << Test::testRemoveMod() << std::endl;
+	}
+
+	//testy dla metod z klasy Handler
+
+	if (!Test::testGet()) {
+		std::cout << Test::testGet() << std::endl;
+	}
+	if (!Test::testAdd()) {
+		std::cout << Test::testAdd() << std::endl;
+	}
+	if (!Test::testRemove()) {
+		std::cout << Test::testRemove() << std::endl;
+	}
+
+	//testy dla metod z klasy MessageHandler
+
+	if (!Test::testGetMessageByDate()) {
+		std::cout << Test::testGetMessageByDate() << std::endl;
+	}
+	if (!Test::testAddMessage()) {
+		std::cout << Test::testAddMessage() << std::endl;
+	}
+	if (!Test::testRemoveMessage()) {
+		std::cout << Test::testRemoveMessage() << std::endl;
+	}
+
+	//testy dla metod z klasy Mod
+
+	if (!Test::testChangeGroupName()) {
+		std::cout << Test::testChangeGroupName() << std::endl;
+	}
+	if (!Test::testAddUser()) {
+		std::cout << Test::testAddUser() << std::endl;
+	}
+	if (!Test::testModRemoveUser()) {
+		std::cout << Test::testModRemoveUser() << std::endl;
+	}
+
+	//testy dla metod z klasy User
+
+	if (!Test::testCreateGroup()) {
+		std::cout << Test::testCreateGroup() << std::endl;
+	}
+	if (!Test::testAddFriend()) {
+		std::cout << Test::testAddFriend() << std::endl;
+	}
+	if (!Test::testRemoveFriend()) {
+		std::cout << Test::testRemoveFriend() << std::endl;
+	}
+	if (!Test::testActualiseDescription()) {
+		std::cout << Test::testActualiseDescription() << std::endl;
+	}
+	if (!Test::testActualiseProfilPicture()) {
+		std::cout << Test::testActualiseProfilPicture() << std::endl;
+	}
+
+	//testy dla metod z klasy UserHandler
+
+	if (!Test::testGetUser()) {
+		std::cout << Test::testGetUser() << std::endl;
+	}
+	if (!Test::testHandlerRemoveUser()) {
+		std::cout << Test::testHandlerRemoveUser() << std::endl;
+	}
+	if (!Test::testUpdateUser()) {
+		std::cout << Test::testUpdateUser() << std::endl;
+	}
 }
 
 int main()
