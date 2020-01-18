dc85dec509e0018a2dc7f384f7a80fceb139ad60
Paweł Duda <pduda.98@gmail.com>
2020-01-18 19:49:58 +0100

    poprawienie bledów kopilacji

--- iscom/iscom/Mod.cpp
+++ iscom/iscom/Mod.cpp
@@ -1,1 +1,16 @@
 #include "Mod.h"
+
+std::string Mod::changeGroupName(std::string groupName, std::string groupId)
+{
+	return std::string();
+}
+
+std::string Mod::addUser(std::string id_user, std::string groupId)
+{
+	return std::string();
+}
+
+std::string Mod::removeUser(std::string id_user, std::string groupId)
+{
+	return std::string();
+}
