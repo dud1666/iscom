0f9ff9e5cd7297a9fce41ab0a3ae7f55b3818150
gawronj <gawron.j@mpcreation.net>
2020-01-18 19:21:18 +0100

    small change

--- iscom/iscom/Handler.h
+++ iscom/iscom/Handler.h
@@ -7,7 +7,7 @@ class Handler
 public:
 	std::string file;
 	nlohmann::json get(std::string id);
-	nlohmann::json change(std::string id, std::map<std::string, std::string> values);
+	nlohmann::json change(std::map<std::string, std::string> values);
 	nlohmann::json remove(std::string id);
 	Handler();
 };
